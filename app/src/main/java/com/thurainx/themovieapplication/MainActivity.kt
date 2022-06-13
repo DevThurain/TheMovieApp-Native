98b876eaa2630e03e4bf289fe470fb3f18ff913c
thurain <thurainbowin.dev@gmail.com>
2022-06-13 20:48:13 +0630

    - setup actionBar

--- app/src/main/java/com/thurainx/themovieapplication/MainActivity.kt
+++ app/src/main/java/com/thurainx/themovieapplication/MainActivity.kt
@@ -2,10 +2,24 @@ package com.thurainx.themovieapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolBar)
+        toolBar.setNavigationIcon(R.drawable.ic_menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_discover,menu)
+        return true
     }
 }
