b0c51cd228c4fd1200799e3d648947820c2d403c
thurain <thurainbowin.dev@gmail.com>
2022-06-16 23:34:18 +0630

    - movie detail About section

--- app/src/main/java/com/thurainx/themovieapplication/activities/MovieDetailActivity.kt
+++ app/src/main/java/com/thurainx/themovieapplication/activities/MovieDetailActivity.kt
@@ -1,10 +1,14 @@
 package com.thurainx.themovieapplication.activities
 
+import android.app.Person
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.thurainx.themovieapplication.R
+import com.thurainx.themovieapplication.viewpods.PersonListViewPod
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
 
@@ -14,8 +18,36 @@ class MovieDetailActivity : AppCompatActivity() {
             return intent
         }
     }
+
+    lateinit var mActorListViewPod: PersonListViewPod
+    lateinit var mCreatorListViewPod: PersonListViewPod
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+
+        setupViewPods()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        flBack.setOnClickListener {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setupViewPods() {
+        mActorListViewPod = vpMovieDetailActorList as PersonListViewPod
+        mCreatorListViewPod = vpMovieDetailCreatorList as PersonListViewPod
+
+        mActorListViewPod.setupPersonViewpod(
+            backgroundColorReference = ContextCompat.getColor(this,R.color.colorPrimary),
+            titleText = getString(R.string.lbl_actors),
+            moreTitleText = ""
+        )
+        mCreatorListViewPod.setupPersonViewpod(
+            backgroundColorReference = ContextCompat.getColor(this,R.color.colorPrimary),
+            titleText = getString(R.string.lbl_creators),
+            moreTitleText = getString(R.string.lbl_more_creators)
+        )
     }
 }
--- app/src/main/java/com/thurainx/themovieapplication/viewpods/PersonListViewPod.kt
+++ app/src/main/java/com/thurainx/themovieapplication/viewpods/PersonListViewPod.kt
@@ -1,8 +1,12 @@
 package com.thurainx.themovieapplication.viewpods
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
+import com.thurainx.themovieapplication.R
 import com.thurainx.themovieapplication.adapters.PersonAdapter
 import kotlinx.android.synthetic.main.viewpod_person_list.view.*
 
@@ -11,9 +15,20 @@ class PersonListViewPod @JvmOverloads constructor(
 ) : RelativeLayout(context, attrs) {
     lateinit var mPersonAdapter: PersonAdapter
     override fun onFinishInflate() {
+        setupPersonRecyclerView()
+        super.onFinishInflate()
+    }
+
+    fun setupPersonViewpod(backgroundColorReference: Int,titleText: String,moreTitleText: String){
+        setBackgroundColor(backgroundColorReference)
+        tvPersonTitle.text = titleText
+        tvMore.text = moreTitleText
+        tvMore.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+    }
+
+    private fun setupPersonRecyclerView(){
         mPersonAdapter = PersonAdapter()
         rvPersonList.adapter = mPersonAdapter
-        super.onFinishInflate()
     }
 
 }
